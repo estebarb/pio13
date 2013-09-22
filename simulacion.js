2529a8238d9df39e01cdc4ea0c01b60079b02e0c
Esteban R <estebarb@gmail.com>
2013-09-21 18:29:19 -0600

    Se completa el objeto estado. Corrección a la creación de objetos.

--- simulacion.js
+++ simulacion.js
@@ -28,21 +28,17 @@ function Evento(tiempo, lambda){
 // contiene los siguientes elementos:
 // HoraCreacion
 // HoraEvento
-// NumRechazos
+// Devoluciones {B:0, C:0}
 // PC Anterior (puede ser la 0, 1, 2 o 3)
-// Tiempo en cola A
-// Tiempo en cola B
-// Tiempo en cola C
-// Tiempo en transmisión
-// Tiempo en procesamiento
-function Mensaje(HoraCreacion, HoraEvento, NumRechazos, PCanterior, TiempoColaA, TiempoColaB, TiempoColaC, TiempoTransmisión, TiempoProcesamiento){
+// Tiempo en colas {A:0, B:0, C:0}
+// Tiempo en transmisión {A:0, B:0, C:0}
+// Tiempo en procesamiento {A:0, B:0, C:0}
+function Mensaje(HoraCreacion, HoraEvento, Devoluciones, PCanterior, TiempoColas, TiempoTransmisión, TiempoProcesamiento){
 	this.HoraCreacion = HoraCreacion;
 	this.HoraEvento = HoraEvento;
-	this.NumRechazos = NumRechazos;
+	this.Devoluciones = Devoluciones;
 	this.PCanterior = PCanterior;
-	this.TiempoColaA = TiempoColaA;
-	this.TiempoColaB = TiempoColaB;
-	this.TiempoColaC = TiempoColaC;
+	this.TiempoColas = TiempoColas;
 	this.TiempoTransmision = TiempoTransmision;
 	this.TiempoProcesamiento = TiempoProcesamiento;
 }
@@ -51,7 +47,7 @@ function Mensaje(HoraCreacion, HoraEvento, NumRechazos, PCanterior, TiempoColaA,
 // son:
 // Mensajes aceptados / rechazados:
 // - Porcentaje de tiempo que pasa ocupado cada procesador
-// - Porcentaje de tiempo que pasa ocupado CPU 1 y 3 con mensajes
+// - Porcentaje de tiempo que pasa ocupado PC 1 y 3 con mensajes
 //   que se rechazan.
 // - Porcentaje de mensajes rechazados
 // - Tiempo promedio en el sistema por cada mensaje
@@ -64,8 +60,56 @@ function Mensaje(HoraCreacion, HoraEvento, NumRechazos, PCanterior, TiempoColaA,
 // Para poder llevar un control de las estadísticas se usará
 // un objeto como el siguiente, separando las estadísticas
 // para los mensajes aceptados o rechazados.
-function Estadisticas(numMsg, tiempoCorriendo, tiempoOcupadoA, )
+function Estadisticas(NumMsg, Reloj, TiempoComputo, TiempoEnSistema, Devoluciones, TiempoColas, TiempoTransmision){
+	this.NumMsg = NumMsg;
+	this.Reloj = Reloj;
+	this.TiempoComputo = TiempoComputo;
+	this.TiempoEnSistema = TiempoEnSistema;
+	this.Devoluciones = Devoluciones;
+	this.TiempoColas = TiempoColas;
+	this.TiempoTransmision = TiempoTransmision;
+}
 
+// La simulación consiste en la aplicación de
+// una función de transformación a un Estado del
+// sistema. El estado del sistema es representado
+// por el siguiente objeto:
+function Estado(Reloj, Colas, Eventos, Estados, Estadisticas){
+	// El estado guarda el reloj del 
+	// estado actual de la simulación:
+	this.Reloj = Reloj;
+	
+	// Se guardan las colas de mensajes que ya 
+	// están esperando para ser atendidos en
+	// una cola para cada computadora.
+	// Tiene la forma {A: [], B: [], C:[]}
+	this.Colas = Colas;
+	
+	// Los eventos del sistema se mantienen
+	// en una lista de "Evento"s posibles.
+	// Es como tener una variable por cada
+	// evento con el tiempo de ocurrencia,
+	// con la diferencia que en una lista
+	// se puede iterar por todos los eventos
+	// fácilmente.
+	this.Eventos = Eventos;
+	
+	// Luego se guardan los estados de cada
+	// CPU. Esto tiene la forma:
+	// Estados = {A:false, B1: false, B2: true, C:false};
+	// true = ocupado, false = libre
+	this.Estados = Estados;
+	
+	// Finalmente se guardan las estadísticas del sistema.
+	// Es necesario mantener estadísticas separadas para
+	// los mensajes aceptados y rechazados.
+	// Las estadísticas se recogen cuando los mensajes son
+	// enviados al cliente o son rechazados por completo.
+	// Tiene la forma:
+	// Estadisticas = {Enviados: new Estadisticas(...), 
+	//                 Rechazados: new Estadisticas(...)}
+	this.Estadisticas = Estadisticas;
+}
 
 
 
